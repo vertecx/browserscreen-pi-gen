e51019f527f37cf301f786324c1fb24955704eeb
vertecx <pjr+git@petr.se>
2017-06-19 15:41:39 +0200

    Removed SSH key regeneration and NOOBS config

--- stage2/01-sys-tweaks/01-run.sh
+++ stage2/01-sys-tweaks/01-run.sh
@@ -1,7 +1,5 @@
 #!/bin/bash -e
 
-install -m 644 files/regenerate_ssh_host_keys.service	${ROOTFS_DIR}/lib/systemd/system/
-install -m 755 files/apply_noobs_os_config		${ROOTFS_DIR}/etc/init.d/
 install -m 755 files/resize2fs_once			${ROOTFS_DIR}/etc/init.d/
 
 install -d						${ROOTFS_DIR}/etc/systemd/system/rc-local.service.d
@@ -13,11 +11,6 @@ install -m 644 files/console-setup   			${ROOTFS_DIR}/etc/default/
 
 on_chroot << EOF
 systemctl disable hwclock.sh
-systemctl disable nfs-common
-systemctl disable rpcbind
-systemctl disable ssh
-systemctl enable regenerate_ssh_host_keys
-systemctl enable apply_noobs_os_config
 systemctl enable resize2fs_once
 EOF
 
@@ -37,5 +30,3 @@ EOF
 on_chroot << EOF
 usermod --pass='*' root
 EOF
-
-rm -f ${ROOTFS_DIR}/etc/ssh/ssh_host_*_key*
