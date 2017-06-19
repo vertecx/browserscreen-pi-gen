6d9e7d18936da6945244118908eb396416615e79
vertecx <pjr+git@petr.se>
2017-06-20 00:19:23 +0200

    Allow exclusion of bootstrap packages

--- stage0/prerun.sh
+++ stage0/prerun.sh
@@ -1,4 +1,4 @@
 #!/bin/bash -e
 if [ ! -d ${ROOTFS_DIR} ]; then
-	bootstrap jessie ${ROOTFS_DIR} http://mirrordirector.raspbian.org/raspbian/
+	bootstrap --exclude=dmidecode jessie ${ROOTFS_DIR} http://mirrordirector.raspbian.org/raspbian/
 fi
