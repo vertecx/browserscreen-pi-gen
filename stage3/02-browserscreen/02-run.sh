dce0d06e06ea7e9852407752638ec038824d3593
vertecx <pjr+git@petr.se>
2017-06-30 17:40:51 +0200

    Initial commit of browserscreen service

--- stage3/02-browserscreen/02-run.sh
+++ stage3/02-browserscreen/02-run.sh
@@ -2,7 +2,10 @@
 
 install -m 644 files/browserscreen.txt ${ROOTFS_DIR}/boot/
 install -m 644 files/.xsession	${ROOTFS_DIR}/home/pi/
+install -m 755 files/browserscreen	${ROOTFS_DIR}/usr/local/bin/
+install -m 644 files/browserscreen.service	${ROOTFS_DIR}/etc/systemd/system/
 
 on_chroot << EOF
 chown pi:pi /home/pi/.xsession
+systemctl enable browserscreen
 EOF
