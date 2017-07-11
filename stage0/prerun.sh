984378ae49d8f75bf518185db2018bc61347e540
vertecx <pjr+git@petr.se>
2017-07-11 18:21:25 +0200

    Exclude more base packages

--- stage0/prerun.sh
+++ stage0/prerun.sh
@@ -1,4 +1,4 @@
 #!/bin/bash -e
 if [ ! -d ${ROOTFS_DIR} ]; then
-	bootstrap --exclude=dmidecode jessie ${ROOTFS_DIR} http://mirrordirector.raspbian.org/raspbian/
+	bootstrap --exclude=aptitude,aptitude-common,bsdmainutils,dmidecode,man-db,manpages,nano,netcat-openbsd,netcat-traditional jessie ${ROOTFS_DIR} http://mirrordirector.raspbian.org/raspbian/
 fi
